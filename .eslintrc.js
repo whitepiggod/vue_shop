8ec70d06f7a30135924fe2713441ec2ef9a88c6d
whitepiggod <baisezhizhu@163.com>
2021-01-20 09:07:24 +0800

    完成了登录功能

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,8 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // 禁用函数后需要空格的语法规则
+    'space-before-function-paren': 0
   }
 }
